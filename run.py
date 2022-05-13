e22ef6d2b6646481c211b1fa901068390a781925
PavelG1307 <100727722+PavelG1307@users.noreply.github.com>
2022-05-14 00:03:04 +0300

    v.1.0.1

--- run.py
+++ run.py
@@ -3,10 +3,12 @@ import socket
 import sqlite3
 import string
 
+
 def connect_db():
     global connection, cursor
     connection = sqlite3.connect('database.db')
     cursor = connection.cursor()
+
 
 def start_server():
     global server
@@ -14,16 +16,17 @@ def start_server():
     server.bind(('127.0.0.1', 8080))
     server.listen(4)
     print('Working...')
-    
-    
-def response_text(content ='', status = 200):
+
+
+def response_text(content='', status=200):
     if status == 200:
         return f'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n{content}'.encode('utf-8')
     elif status == 302:
         return f'HTTP/1.1 302 Found\r\nLocation: {content}'.encode('utf-8')
     elif status == 404:
         return f'HTTP/1.1 404 Not Found'.encode('utf-8')
-    
+
+
 def check_url(url):
     cursor.execute(f"SELECT key FROM Url_and_key WHERE url LIKE '{url}';")
     result = cursor.fetchall()
@@ -31,11 +34,13 @@ def check_url(url):
         return None
     return result[0][0]
 
+
 def check_key(key):
     cursor.execute(f"SELECT COUNT(*) FROM Url_and_key WHERE key LIKE '{key}';")
     if cursor.fetchall()[0][0] == 0:
         return True
     return False
+
 
 def add_url(url):
     key = check_url(url)
@@ -45,6 +50,7 @@ def add_url(url):
         connection.commit()
     return key
 
+
 def get_url(key):
     cursor.execute(f"SELECT url FROM Url_and_key WHERE key LIKE '{key}';")
     url = cursor.fetchall()
@@ -52,20 +58,25 @@ def get_url(key):
         return url[0][0]
     return None
 
+
 def generate_key():
-    dictionary = list(string.ascii_lowercase + string.ascii_uppercase) + ['0','1','2','3','4','5','6','7','8','9']
+    dictionary = list(string.ascii_lowercase + string.ascii_uppercase) + ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     key = ''
     for i in range(8):
-        key+=random.choice(dictionary)
+        key += random.choice(dictionary)
     if check_key(key):
         return(key)
     else:
         generate_key()
         # По хорошему надо сделать ограничение от случая, когда будут заняты все ключи
         # Но вероятность такой ситуации в наших масштабах около нуля
-    
+
+
 def parse_request(request_data):
-    data = request_data.split(' ')[1]
+    try:
+        data = request_data.split(' ')[1]
+    except Exception:
+        return '', 404
     if data[1] == 'a':
         url = data.split('/a/?url=')[1]
         key = add_url(url)
@@ -73,15 +84,16 @@ def parse_request(request_data):
     elif data[1] == 's':
         key = data.split('/s/')[1]
         url = get_url(key)
-        return url, 302
-    return '', 200
+        if url is not None:
+            return url, 302
+    return '', 404
+
 
 def handl():
     client_socket, address = server.accept()
     data = client_socket.recv(1024).decode('utf-8')
     content, status = parse_request(data)
-    client_socket.send(response_text(content = content, status = status))
-
+    client_socket.send(response_text(content=content, status=status))
 
 
 def main():
@@ -90,9 +102,9 @@ def main():
     while(True):
         handl()
 
+
 if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
         pass
-    
